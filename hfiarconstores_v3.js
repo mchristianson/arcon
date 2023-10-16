557d0a82090a695c839eaecba1bac64339cbe88d
MattChristianson <Matthew.Christianson@target.com>
2023-10-16 18:44:40 -0500

    New version

--- hfiarconstores_v3.js
+++ hfiarconstores_v3.js
@@ -1,7 +1,10 @@
 const shopArconCustom = window.shopArconCustom || (function (document, window, $) {
     const app = {
         init: () => {
-            $('input#shipping_misc1').remove();
+            app.waitForElement('input#shipping_misc1')
+                .then((ele) => {
+                    ele.remove();
+                })
             const $dropDown = $('<select name="shipping_misc1" style="padding: 3px;" required>')
                 .append('<option></option>')
                 .append('<option>Bingham AG Services</option>')
@@ -19,10 +22,27 @@ const shopArconCustom = window.shopArconCustom || (function (document, window, $
                 .append('<option>Teton Petroleum Transport</option>');
 
             $('#shipping_misc1_container').append($dropDown);
+        },
+        waitForElement: (selector) => {
+            return new Promise(resolve => {
+                if (document.querySelector(selector)) {
+                    return resolve(document.querySelector(selector));
+                }
+
+                const observer = new MutationObserver(mutations => {
+                    if (document.querySelector(selector)) {
+                        observer.disconnect();
+                        resolve(document.querySelector(selector));
+                    }
+                });
+
+                observer.observe(document.body, {
+                    childList: true,
+                    subtree: true
+                });
+            });
         }
     };
     return app;
 }(document, window, jQuery));
-jQuery(function() {
-    shopArconCustom.init();
-});
+shopArconCustom.init();
