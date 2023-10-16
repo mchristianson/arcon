05fed86e00ce769680d3e3eac94df3b480a8923e
Matt Christianson <christianson.matt@gmail.com>
2023-10-16 15:47:35 -0500

    Update hfiarconstores.js

--- hfiarconstores.js
+++ hfiarconstores.js
@@ -1,2 +1,5 @@
-const misc1 = document.getElementById('shipping_misc1_container');
-console.log('It is working', misc1);
+jQuery( document ).ready(function() {
+   console.log( "ready!" );
+   const misc1 = document.getElementById('shipping_misc1_container');
+   console.log('It is working', misc1);
+});
